0075a84651c611d34ad6dfc436032f741c7516ee
Michael Francis <michaelmugendi1707@gmail.com>
2026-01-14 13:38:10 +0300

    Update: Pick winner

--- lottery-contract/programs/lottery-contract/src/lib.rs
+++ lottery-contract/programs/lottery-contract/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::{
     prelude::*,
-    solana_program::{clock::Clock, program::invoke, system_instruction::transfer},
+    solana_program::{ program::invoke, system_instruction::transfer},
 };
 
 mod constants;
@@ -12,6 +12,7 @@ declare_id!("2X4c4bynxsEGVHMJTtPauRa8RRxbjw7PugorBYw8KWZo");
 
 #[program]
 pub mod lottery_contract {
+
     use super::*;
 
     pub fn init_master(_ctx: Context<InitMaster>) -> Result<()> {
@@ -42,7 +43,49 @@ pub mod lottery_contract {
         Ok(())
     }
 
-    pub fn buy_ticket(ctx: Context<BuyTicket>, lottery_id: u32) -> Result<()> {
+    pub fn buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
+        let lottery = &mut ctx.accounts.lottery;
+        let ticket = &mut ctx.accounts.ticket;
+        let buyer = &ctx.accounts.buyer;
+        require!(lottery.is_active, LotteryError::LotteryNotActive);
+        require!(lottery.claimed == false, LotteryError::LotteryClaimed);
+        require!(lottery.tickets_sold < lottery.max_tickets, LotteryError::LotterySoldOut);
+        // Transfer SOL from buyer to lottery prize pool
+        invoke(
+            &transfer(
+                &buyer.key(),
+                &lottery.key(),
+                lottery.ticket_price
+            ),
+            &[buyer.to_account_info(), lottery.to_account_info()]
+        )?;
+
+        lottery.tickets_sold += 1;
+        lottery.prize_pool += lottery.ticket_price;
+        lottery.last_ticket_id += 1;
+
+        ticket.id = lottery.last_ticket_id;
+        ticket.lottery_id = lottery.id;
+        ticket.buyer = *buyer.key;
+
+        msg!("Ticket: {} bought by: {} for lottery: {}", ticket.id, ticket.buyer, lottery.id);
+
+        Ok(())
+    }
+
+    pub fn draw_winner(ctx: Context<PickWinner>, _lottery_id: u32) -> Result<()> {
+        let lottery = &mut ctx.accounts.lottery;
+        require!(lottery.is_active, LotteryError::LotteryNotActive);
+        require!(lottery.tickets_sold > 0, LotteryError::NoTicketsSold);
+        require!(lottery.winner_id.is_none(), LotteryError::WinnerAlreadyPicked);
+
+        // Pick a random winner
+        let winner_id = lottery.last_ticket_id;
+        lottery.winner_id = Some(winner_id);
+
+        msg!("Winner picked for lottery: {}", lottery.id);
+
+
         Ok(())
     }
 }
@@ -109,7 +152,7 @@ pub struct BuyTicket<'info> {
     #[account(
         init,
         payer = buyer,
-        space = 8 + 4 + 32,
+        space = 8 + 8 + 4 + 32,
         seeds = [TICKET_SEED, lottery.key().as_ref(), &[lottery.last_ticket_id as u8 + 1]],
         bump
     )]
@@ -121,6 +164,29 @@ pub struct BuyTicket<'info> {
 
 #[account]
 pub struct Ticket {
+    pub id: u32,
     pub lottery_id: u32,
     pub buyer: Pubkey,
+}
+
+#[error_code]
+pub enum LotteryError {
+    #[msg("The lottery is not active.")]
+    LotteryNotActive,
+    #[msg("The lottery is sold out.")]
+    LotterySoldOut,
+    #[msg("The lottery is claimed.")]
+    LotteryClaimed,
+    #[msg("No tickets have been sold for this lottery.")]
+    NoTicketsSold,
+    #[msg("A winner has already been picked for this lottery.")]
+    WinnerAlreadyPicked,
+}
+
+#[derive(Accounts)]
+#[instruction(lottery_id: u32)]
+pub struct PickWinner<'info> {
+    #[account(mut, seeds = [LOTTERY_SEED, &[lottery_id as u8]], has_one = authority, bump)]
+    pub lottery: Account<'info, Lottery>,
+    pub authority: Signer<'info>,
 }
